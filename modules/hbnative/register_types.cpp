cbe71b61dfa57c5167e1764225dd896f351a2bef
Álex Román Núñez <eirexe123@gmail.com>
2021-10-28 01:52:57 +0200

    Add experimental romanization module

--- modules/hbnative/register_types.cpp
+++ modules/hbnative/register_types.cpp
@@ -33,10 +33,12 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
+#include "hbnative_singleton.h"
 #include "ph_audio_stream_editor.h"
 #include "ph_audio_stream_preview.h"
 
 static PHAudioStreamPreviewGenerator *ph_ptr = NULL;
+static HBNativeSingleton *hbnative_ptr = NULL;
 
 void register_hbnative_types() {
 	ph_ptr = memnew(PHAudioStreamPreviewGenerator);
@@ -44,8 +46,12 @@ void register_hbnative_types() {
 	ClassDB::register_class<PHAudioStreamEditor>();
 	ClassDB::register_class<PHAudioStreamPreview>();
 	Engine::get_singleton()->add_singleton(Engine::Singleton("PHAudioStreamPreviewGenerator", PHAudioStreamPreviewGenerator::get_singleton()));
+	hbnative_ptr = memnew(HBNativeSingleton);
+	ClassDB::register_class<HBNativeSingleton>();
+	Engine::get_singleton()->add_singleton(Engine::Singleton("HBNativeSingleton", HBNativeSingleton::get_singleton()));
 }
 
 void unregister_hbnative_types() {
 	memdelete(ph_ptr);
+	memdelete(hbnative_ptr);
 }
