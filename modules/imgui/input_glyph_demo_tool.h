42cbc82b29887ec11908020753b61f66c91f087f
Álex Román Núñez <eirexe123@gmail.com>
2023-09-19 19:11:53 +0200

    Update input glyph demo tool header

--- modules/imgui/input_glyph_demo_tool.h
+++ modules/imgui/input_glyph_demo_tool.h
@@ -7,6 +7,7 @@
 
 #include "modules/imgui/godot_imgui.h"
 #include "modules/input_glyphs/input_glyphs.h"
+#include "modules/input_glyphs/input_glyphs_constants.h"
 #include "modules/input_glyphs/input_glyphs_singleton.h"
 #include "modules/steamworks/steam_input.h"
 #include <iterator>
@@ -14,18 +15,18 @@
 class InputGlyphDemoTool : public GodotImGuiTool {
 	GDCLASS(InputGlyphDemoTool, GodotImGuiTool);
 	struct GlyphDebugInfo {
-		InputOrigin origin;
+		InputGlyphsConstants::InputOrigin origin;
 		Ref<Texture2D> texture;
 		int style;
 	};
 
-	int input_type_idx = InputType::XBOX_ONE_CONTROLLER;
+	int input_type_idx = InputGlyphsConstants::XBOX_ONE_CONTROLLER;
 	int glyph_size = InputGlyphSize::GLYPH_SIZE_SMALL;
 
 	// 3 themes, knockout light and dark
 	// we also add 3 spaces per-theme for the different variants of ABXY
 	// (GLYPH_STYLE_NEUTRAL_COLOR_ABXY, GLYPH_STYLE_SOLID_ABXY, GLYPH_STYLE_NEUTRAL_COLOR_ABXY + GLYPH_STYLE_SOLID_ABXY)
-	static const int GLYPH_COUNT = InputOrigin::INPUT_ORIGIN_COUNT + (4 * 3);
+	static const int GLYPH_COUNT = InputGlyphsConstants::INPUT_ORIGIN_COUNT + (4 * 3);
 	static const int THEME_COUNT = 3;
 
 	const char *THEME_NAMES[THEME_COUNT] = {
