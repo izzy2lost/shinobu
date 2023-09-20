f0405fe7ff81930bd2f0df1b2f5b4ad7db6f12d9
Álex Román Núñez <eirexe123@gmail.com>
2023-09-20 20:01:09 +0200

    Update input glyphs and fix demotool building

--- modules/imgui/input_glyph_demo_tool.cpp
+++ modules/imgui/input_glyph_demo_tool.cpp
@@ -8,7 +8,7 @@
 void InputGlyphDemoTool::reload_glyphs() {
 	Ref<InputGlyphsSource> source = InputGlyphsSource::create();
 
-	InputType input_type = (InputType)input_type_idx;
+	InputGlyphsConstants::InputType input_type = (InputGlyphsConstants::InputType)input_type_idx;
 
 	for (int theme_i = 0; theme_i < THEME_COUNT; theme_i++) {
 		int base_style = theme_i;
@@ -22,16 +22,16 @@ void InputGlyphDemoTool::reload_glyphs() {
 		for (int i = 0; i < 4 * 4; i++) {
 			int style = ABXY_STYLES[i / 4];
 			GlyphDebugInfo info;
-			info.origin = (InputOrigin)(i % 4);
+			info.origin = (InputGlyphsConstants::InputOrigin)(i % 4);
 			info.texture = source->get_input_glyph(input_type, info.origin, style, (InputGlyphSize)glyph_size);
 			info.style = style;
 			glyph_infos[theme_i][i] = info;
 		}
 
 		// Now do the rest
-		for (int i = 4; i < InputOrigin::INPUT_ORIGIN_COUNT; i++) {
+		for (int i = 4; i < InputGlyphsConstants::InputOrigin::INPUT_ORIGIN_COUNT; i++) {
 			GlyphDebugInfo info;
-			info.origin = (InputOrigin)i;
+			info.origin = (InputGlyphsConstants::InputOrigin)i;
 			info.texture = source->get_input_glyph(input_type, info.origin, base_style, (InputGlyphSize)glyph_size);
 			info.style = base_style;
 			glyph_infos[theme_i][(4 * 3) + i] = info;
@@ -57,7 +57,7 @@ void InputGlyphDemoTool::draw_glyphs(int p_theme_i) {
 		}
 		if (ImGui::IsItemHovered()) {
 			ImGui::BeginTooltip();
-			ImGui::TextUnformatted(InputGlyphTables::_debug_input_origin_names[glyph_infos[p_theme_i][i].origin]);
+			ImGui::TextUnformatted(InputGlyphsConstants::_debug_input_origin_names[glyph_infos[p_theme_i][i].origin]);
 			String path = tex->get_meta("glyph_path", "");
 			path = path.get_file();
 			ImGui::TextUnformatted(path.utf8().get_data());
@@ -99,7 +99,7 @@ void InputGlyphDemoTool::draw_ui() {
 		"Steam Deck Controller",
 	};
 
-	static_assert(std::size(input_type_names) == InputType::INPUT_TYPE_MAX);
+	static_assert(std::size(input_type_names) == InputGlyphsConstants::InputType::INPUT_TYPE_MAX);
 
 	static const char *input_size_names[] = {
 		"Small - 32x32",
@@ -113,7 +113,7 @@ void InputGlyphDemoTool::draw_ui() {
 	for (int i = 0; i < connected.size(); i++) {
 		int number = connected[i];
 		ImGui::Text("%d %d %s", i, number, Input::get_singleton()->get_joy_name(number).utf8().get_data());
-		InputType it = InputGlyphsSingleton::get_singleton()->glyph_source->identify_joy(number);
+		InputGlyphsConstants::InputType it = InputGlyphsSingleton::get_singleton()->glyph_source->identify_joy(number);
 		ImGui::Text("%d %s", it, input_type_names[it]);
 		if (Input::get_singleton()->is_joy_button_pressed(number, JoyButton::A)) {
 			ImGui::SameLine();
@@ -122,7 +122,7 @@ void InputGlyphDemoTool::draw_ui() {
 		ImGui::Separator();
 	}
 	if (ImGui::BeginCombo("Input Type", input_type_names[input_type_idx])) {
-		for (int i = 0; i < InputType::INPUT_TYPE_MAX; i++) {
+		for (int i = 0; i < InputGlyphsConstants::InputType::INPUT_TYPE_MAX; i++) {
 			if (ImGui::Selectable(input_type_names[i], i == input_type_idx)) {
 				input_type_idx = i;
 				reload_glyphs();
