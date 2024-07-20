13f782d221592eebb737521271f762792f6364d9
Álex Román Núñez <eirexe123@gmail.com>
2024-07-20 17:41:21 +0200

    Monorepo preparations

--- modules/hbnative/register_types.h
+++
@@ -1,8 +0,0 @@
-#ifndef HBNATIVE_REGISTER_TYPES_H
-#define HBNATIVE_REGISTER_TYPES_H
-
-#include "modules/register_module_types.h"
-
-void initialize_hbnative_module(ModuleInitializationLevel p_level);
-void uninitialize_hbnative_module(ModuleInitializationLevel p_level);
-#endif
--- modules/shinobu/register_types.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHINOBU_REGISTER_TYPES_H
-#define SHINOBU_REGISTER_TYPES_H
-#include "modules/register_module_types.h"
-
-void initialize_shinobu_module(ModuleInitializationLevel p_level);
-void uninitialize_shinobu_module(ModuleInitializationLevel p_level);
-#endif
